86a60550d23413a3c567d0e465359241a12385e5
surajondev <surajvishwakarma625@gmail.com>
2022-03-06 12:15:24 +0530

    initial commit

--- components/Work.js
+++ components/Work.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Heading, Button, Stack } from "@chakra-ui/react"
 import WorkContainer from "./containers/WorkContainer"
 import data from '../data/workData.js'
@@ -6,29 +6,30 @@ import data from '../data/workData.js'
 const Work = () => {
     const [active, setActive] = useState("all")
 
-    let newData = data
+    let writingData = data.filter((data)=> data.tag==="Writing")
+    let webData = data.filter((data)=> data.tag==="Web Dev")
 
-    const writingActive = () => {
-        newData = data.filter((data)=> data.tag==="Writing")
-        console.log(newData)
-    }
+    let activeButton = {color:"black", borderColor:'#ffa97a', backgroundColor:"#ffa97a", variant:'solid', size:"md"}
+    let disableButton = {color:"#ffa97a", borderColor:'#ffa97a',  variant:'outline', size:"md"}
 
     return(
         <div className="aboutContainer">
             <Heading style={{color:"#ffa97a"}}>Recent Work</Heading>
             <p style={{color:"#767676", fontWeight:"bold"}}>My portfolio</p>
             <Stack direction='row' spacing={4} align='center' style={{margin:"1.5em 0"}}>
-                <Button className="About" color="black" borderColor='#ffa97a' backgroundColor="#ffa97a" variant='solid' size="md" _hover={{ bg: '#cc8762' }}>
+                <Button onClick={() => setActive("all")}  isDisabled={true} _disabled={disableButton}  _active={activeButton}  _hover={{ bg: '#cc8762' }}>
                     All
                 </Button>
-                <Button onClick={writingActive} className="downloadCV" borderColor='#ffa97a' color="#ffa97a" variant='outline' size="md" _hover={{ borderColor:"#cc8762", color:"#cc8762" }}>
+                <Button onClick={() => setActive("writing")} isActive={true} _active={disableButton} _hover={{ borderColor:"#cc8762", color:"#cc8762" }}>
                     Writing
                 </Button>
-                <Button className="downloadCV" borderColor='#ffa97a' color="#ffa97a" variant='outline' size="md" _hover={{ borderColor:"#cc8762", color:"#cc8762" }}>
+                <Button onClick={() => setActive("webdev")} color="#ffa97a" borderColor='#ffa97a'  variant='outline' size="md" _hover={{ borderColor:"#cc8762", color:"#cc8762" }}>
                     Web Dev
                 </Button>
             </Stack>
-            {<WorkContainer data={newData}/>}
+            {active ==="all" && <WorkContainer data={data}/>}
+            {active ==="writing" && <WorkContainer data={writingData}/>}
+            {active ==="webdev" && <WorkContainer data={webData}/>}
         </div>
     )
 }
